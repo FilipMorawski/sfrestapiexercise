c257f99855359091ea2cd1e6d65b1af3edef7657
Filip Morawski <morawski.filip@o2.pl>
2020-05-10 17:32:32 +0200

    With Apex back-end and LWC front-end solution

--- force-app/main/default/lwc/testObject/testObject.js
+++ force-app/main/default/lwc/testObject/testObject.js
@@ -1,16 +1,8 @@
 import { LightningElement, api, track, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
 import { refreshApex } from '@salesforce/apex';
-
-import NUMBER_FIELD from '@salesforce/schema/TestCustomObject__c.NumberValue__c';
-import NAME_FIELD from '@salesforce/schema/TestCustomObject__c.Name';
-import PICKLIST_FIELD from '@salesforce/schema/TestCustomObject__c.TestPicklistValue__c';
-import CHECKBOX_FIELD from '@salesforce/schema/TestCustomObject__c.TestCheckboxValue__c';
-
 import { subscribe, onError } from 'lightning/empApi';
 
-import getObject from '@salesforce/apex/TestObjectRetriever.getObject';
-
+import getTestObject from '@salesforce/apex/TestObjectController.getTestObject';
 
 export default class TestObject extends LightningElement {
 
@@ -20,43 +12,30 @@ export default class TestObject extends LightningElement {
  @track object;
  @track wiredRecords;
 
-@wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD, NUMBER_FIELD, CHECKBOX_FIELD, PICKLIST_FIELD]})
-getRecord(result) {
+ @wire(getTestObject, {id : '$recordId'})
+ getTestObject(result) {
 
-     if(!this.subscription) {
-         var channelName = '/data/ChangeEvents';
-
-                const messageCallback = function(response) {
-                    if(response.data.payload.ChangeEventHeader.recordIds.length == 1
-                        && response.data.payload.ChangeEventHeader.recordIds[0] == this.recordId) {
-
-                        console.log('REFRESH DATA');
+    if(!this.subscription) {
+        var channelName = '/data/ChangeEvents';
+            const messageCallback = function(response) {
+                if(response.data.payload.ChangeEventHeader.recordIds.length == 1
+                    && response.data.payload.ChangeEventHeader.recordIds[0] == this.recordId) {
                         refreshApex(this.wiredRecords);
                     }
                 }.bind(this)
 
                 subscribe(channelName, -1, messageCallback).then(response => {
-                    console.log('ChannelName : ' + channelName);
                     this.subscription = response;
                 });
      }
 
-     this.wiredRecords = result;
-     if (result.data) {
-        var obj = {};
-        obj[NUMBER_FIELD.fieldApiName] = result.data.fields[NUMBER_FIELD.fieldApiName].value;
-        obj[NAME_FIELD.fieldApiName] = result.data.fields[NAME_FIELD.fieldApiName].value;
-        obj[PICKLIST_FIELD.fieldApiName] = result.data.fields[PICKLIST_FIELD.fieldApiName].value;
-        obj[CHECKBOX_FIELD.fieldApiName] = result.data.fields[CHECKBOX_FIELD.fieldApiName].value;
-        this.object = obj;
+    this.wiredRecords = result;
+    if (result.data) {
+        this.object = result.data;
     } else if (result.error) {
-         console.error('ERROR => ', JSON.stringify(error)); // handle error properly
+         console.error('ERROR => ', JSON.stringify(error));
      }
  }
-
-handleClick() {
-    registerChannel();
-}
 
 }
 
